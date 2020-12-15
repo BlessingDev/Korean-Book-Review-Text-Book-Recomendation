27963e018ae1bad3126c3e2301c3fe1dda66505e
Junho Park <blessingdev@khu.ac.kr>
2020-12-15 12:42:57 +0900

    20/12/15 프로그램 작성

---
+++ book_recommender.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import scipy.sparse
+import soynlp.hangle as soyh
+
+class BookRecommender:
+    def __init__(self):
+         DATA = Path('.').resolve() / "Data"
+
+         self.book_table = pd.read_csv(DATA / "book1.csv", index_col=[0])
+         self.book_table = self.book_table.set_index("Unnamed: 0.1")
+         self.rv_table = pd.read_csv(DATA / "review1_pre.csv", index_col=[0])
+         self.rv_table = self.rv_table.drop_duplicates(['Author', 'BookCode'], keep='first')
+         self.rv_table = self.rv_table[~pd.isna(self.rv_table["Author"])]
+
+         self.rv_book_pivot = self.rv_table.pivot(index='Author', columns='BookCode', values='SumRate')
+         self.rv_book_pivot = self.rv_book_pivot.fillna(0)
+         self.us_list = self.rv_book_pivot.index
+         self.book_list = self.rv_book_pivot.columns
+
+         self.pred_res = scipy.sparse.load_npz(DATA / "pred_tfidf_comb1.npz")
+         self.pred_res = pd.DataFrame(self.pred_res.todense(), index=self.us_list, columns=self.book_list)
+
+    def search_book(self, search_text, n=10):
+        search_table = self.book_table.copy()
+        search_table["JamoEditDis"] = [soyh.jamo_levenshtein(t, search_text) for t in search_table["BookTitle"].tolist()]
+        search_table["levEditDis"] = [soyh.levenshtein(t, search_text) for t in search_table["BookTitle"].tolist()]
+        search_table["EditDis"] = np.mean(search_table[["JamoEditDis", "levEditDis"]], axis=1)
+        search_table = search_table.sort_values("EditDis")
+        return search_table.head(n)
+
+    def sample(self, n):
+        user_table = self.rv_table.groupby(["Author"]).count()
+        return self.book_table.sample(n), user_table[["Title"]].sample(n)
+
+    def recommend_book(self, user, n=10):
+        if user in self.us_list:
+            pred_score = self.pred_res.loc[user, :]
+            pred_score = pred_score.T
+            pred_score = pred_score.sort_values(ascending=False)
+            pred_score = pred_score.reset_index()
+            pred_book = pd.merge(pred_score, self.book_table, left_on="BookCode", right_on="ISBN", how="left")
+            pred_book = pred_book.rename(columns={user:"PredScore"})
+            pred_book = pred_book.drop(["BookCode"], axis=1)
+
+            real_score = self.rv_book_pivot.loc[user, :]
+            real_score = real_score.T
+            real_score = real_score.reset_index()
+            pred_book = pd.merge(pred_book, real_score, left_on="ISBN", right_on="BookCode", how="left")
+            pred_book = pred_book.rename(columns={user: "RealScore"})
+
+            return pred_book[pred_book["RealScore"] == 0].iloc[:n, :]
+        else:
+            print("존재하지 않는 유저입니다.")
--- main.py
+++ main.py
@@ -1,28 +1,56 @@
-import numpy as np
 import pandas as pd
-import scipy.sparse
-import soynlp.hangle as soyh
+import book_recommender
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 def show_menu():
     menu_list = [
         '책 검색',
-        '랜덤 책 샘플',
-        '유저 검색',
+        '랜덤 책, 유저 샘플',
         '책 추천'
     ]
 
     print("------리뷰 기반 책 추천기------")
     for i, m in enumerate(menu_list):
         print("{0}. {1}".format(i + 1, m))
+    print("-1. 프로그램 종료")
     print("--------------------------------------")
 
-def book_search(search_text):
+def book_search(search_text, br):
+    res = br.search_book(search_text)
+    print(res[["BookTitle", "BookAuthor", "Category-2"]])
 
+def sample_data(n, br):
+    book, us = br.sample(n)
+    print(book[["BookTitle", "BookAuthor", "Category-2"]])
+    print(us)
+
+def book_recommend(user, n, br):
+    res = br.recommend_book(user, n)
+    print(res[["BookTitle", "BookAuthor", "ISBN", "PredScore"]])
 
 if __name__ == "__main__":
-    show_menu()
-    c = input("메뉴를 선택하세요: ")
+    br = book_recommender.BookRecommender()
 
-    if c == '1':
-        search_text = input("검색어를 입력하세요: ")
+    program = True
+    while program:
+        show_menu()
+        c = input("메뉴를 선택하세요: ")
+
+        if c == '1':
+            search_text = input("검색어를 입력하세요: ")
+            book_search(search_text, br)
+        elif c == '2':
+            n = input("몇 개를 샘플하시겠습니까? ")
+            if n.isdigit():
+                n = int(n)
+                sample_data(n, br)
+            else:
+                print("숫자가 아닙니다")
+        elif c == '3':
+            user = input("추천을 받을 유저를 입력하세요: ")
+            book_recommend(user, 10, br)
+        elif c == '-1':
+            print("프로그램을 종료합니다.")
+            program=False
